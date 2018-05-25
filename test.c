622506d4a6655b939f8aa00ad13f08cb4cc347f6
hiro <narajiji1117@gmail.com>
2018-05-25 15:06:03 +0900

    create new file

--- test.c
+++ test.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+
+#define BUFFER_SIZE 2048
+
 int main()
 {
 	char* hoge[]={"hh","jjrg","gkk"};
 	for(int i=0;i<3;i++){
 		printf("%s,strlen():%lu\n",hoge[i],strlen(hoge[i]));
 	}
+	int a[BUFFER_SIZE];
+	printf("%d\n",sizeof a/sizeof(int));
 }	
